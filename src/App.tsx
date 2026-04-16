4f0c8e9d48ed426dca8bbaa630e7791c81df0a85
Gabriel Viana <gaa.vianaa@gmail.com>
2026-04-16 20:41:13 -0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+
+function App() {
+  return (
+    <div className="min-h-screen bg-zinc-900 text-white">
+      <h1 className="text-4xl font-bold p-10">
+        Meu Portfólio 🚀
+      </h1>
+    </div>
+  )
+}
+
+export default App
+
+
