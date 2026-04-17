7d603febc1ce37fcadfc38efca25fe028f77215a
Gabriel Viana <gaa.vianaa@gmail.com>
2026-04-17 16:10:43 -0300

    config tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,31 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+       colors: {
+    fundo: "rgb(var(--color-fundo) / <alpha-value>)",
+    texto: "rgb(var(--color-texto) / <alpha-value>)",
+
+    card: "rgb(var(--color-card) / <alpha-value>)",
+
+    primaria: "rgb(var(--color-primaria) / <alpha-value>)",
+    "primaria-topo": "rgb(var(--color-primaria-topo) / <alpha-value>)",
+
+    secundaria: "rgb(var(--color-secundaria) / <alpha-value>)",
+    "secundaria-topo": "rgb(var(--color-secundaria-topo) / <alpha-value>)",
+
+    off: "rgb(var(--color-off) / <alpha-value>)",
+    "off-texto": "rgb(var(--color-off-texto) / <alpha-value>)",
+
+    border: "rgb(var(--color-border) / <alpha-value>)",
+
+    destaque: "rgb(var(--color-destaque) / <alpha-value>)",
+    superficie: "rgb(var(--color-superficie) / <alpha-value>)",
+  },
+      borderRadius: {
+        DEFAULT: "var(--radius)",
+      },
+    }
   },
   plugins: [],
 }
