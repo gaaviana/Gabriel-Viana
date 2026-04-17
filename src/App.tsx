6eff0aa2dbd83d2b1ecf525e494e176c835c14a1
Gabriel Viana <gaa.vianaa@gmail.com>
2026-04-16 21:36:44 -0300

    testa tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
-import Hero from './sections/Hero'
-import About from './sections/About'
-import Projects from './sections/Projects'
-import Skills from './sections/Skills'
-import Contact from './sections/Contact'
+import Hero from '@/sections/Hero'
+import About from '@/sections/About'
+import Projects from '@/sections/Projects'
+import Skills from '@/sections/Skills'
+import Contact from '@/sections/Contact'
 
 function App() {
   return (
-    <div className="min-h-screen bg-zinc-900 text-white">
-      <h1 className="text-4xl font-bold p-10">
+    <div>
+      <h1>
         Meu Portfólio 🚀
       </h1>
       <Hero />
